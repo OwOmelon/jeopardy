4351a88b25dba6715d693f392ce3574ffda07973
OwOmelon <96761586+OwOmelon@users.noreply.github.com>
2023-09-19 09:53:58 +0800

    Delete src directory

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
-import "./style.css";
-import App from "./App.vue";
-
-const pinia = createPinia();
-const app = createApp(App);
-
-app.use(pinia).mount("#app");
