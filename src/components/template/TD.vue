2cb3e6b2af21e2a26ce1e45949e54792309ef3ee
melon <cccaaarrrlll99@gmail.com>
2023-09-26 16:10:40 +0800

    update td styles and text display

--- src/components/template/TD.vue
+++ src/components/template/TD.vue
@@ -18,12 +18,17 @@ const emit = defineEmits<{
 	"on-mouse-leave": [];
 }>();
 
-const missingData = computed<boolean>(() => {
-	return props.data.question && props.data.answer ? false : true;
+const missingDataStyles = computed<string>(() => {
+	return template.cellHasMissingData(props.rowID, props.columnID)
+		? `td-missing-data ${template.editing ? "" : "pointer-events-none"}`
+		: "";
 });
 
 const textDisplay = computed<number | string>(() => {
-	if (!template.editing) return missingData.value ? "" : props.data.points;
+	if (!template.editing)
+		return template.cellHasMissingData(props.rowID, props.columnID)
+			? ""
+			: props.data.points;
 
 	const question = props.data.question || "Add Question";
 	const answer = props.data.answer || "Add Answer";
@@ -34,14 +39,7 @@ const textDisplay = computed<number | string>(() => {
 
 <template>
 	<td
-		:class="[
-			{ 'td-missing-data': missingData },
-			{
-				'hover:bg-red-400 hover:text-white grid place-items-center !text-lg':
-					!template.editing,
-			},
-			'text-xs',
-		]"
+		:class="[{ 'td-playing': !template.editing }, missingDataStyles, 'text-xs']"
 		@mouseenter="emit('on-mouse-enter', props.rowID, props.columnID)"
 		@mouseleave="emit('on-mouse-leave')"
 		@click="
@@ -54,3 +52,17 @@ const textDisplay = computed<number | string>(() => {
 		<p>{{ textDisplay }}</p>
 	</td>
 </template>
+
+<style scoped lang="postcss">
+td {
+	@apply h-20 cursor-pointer rounded bg-stone-300 p-2 text-stone-600 shadow !shadow-black/30 transition-[background-color,_color,_box-shadow,_transform] hover:-translate-y-2 hover:scale-105 hover:shadow-lg active:scale-100;
+}
+
+.td-missing-data {
+	@apply !bg-stone-500 !text-stone-100;
+}
+
+.td-playing {
+	@apply grid place-items-center !text-lg hover:bg-red-400 hover:text-white;
+}
+</style>
