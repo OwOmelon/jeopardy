4476c37c5fe1a4678f1c10fabae0a38260616624
melon <cccaaarrrlll99@gmail.com>
2023-10-11 15:33:28 +0800

    moved to the right of screen

--- src/components/MainMenuModal/ShowMainMenuBtn.vue
+++ src/components/MainMenuModal/ShowMainMenuBtn.vue
@@ -5,7 +5,7 @@ const modals = useModalStore();
 </script>
 
 <template>
-	<button type="button" class="fixed left-5 top-5 grid aspect-square place-items-center rounded bg-black/50 p-5 text-xl text-white transition-transform hover:scale-110"
+	<button type="button" class="fixed right-2 top-2 grid aspect-square place-items-center rounded bg-black/50 p-5 text-xl text-white transition-transform hover:scale-110"
 		@click="modals.mainMenu = !modals.mainMenu"
 	>
 		<span class="absolute">||</span>
