f038e953e1a82ad6fb4d3249a17b2a7632be787e
melon <cccaaarrrlll99@gmail.com>
2023-10-24 15:34:56 +0800

    styled

--- src/components/template/IsEmptyFallback.vue
+++ src/components/template/IsEmptyFallback.vue
@@ -1,14 +1,20 @@
 <script setup lang="ts">
 import { useTemplateStore } from "@/stores/template";
+import { Icon } from "@iconify/vue";
 
 const template = useTemplateStore();
 </script>
 
 <template>
-	<div class="flex flex-col gap-3">
-		<p>you template is empty D:</p>
-		<button type="button" @click="template.editing = true">
+	<div class="flex flex-col items-center gap-3">
+		<p class="text-3xl font-bold">you template is empty D:</p>
+		<button
+			type="button"
+			class="flex items-center gap-3 rounded bg-red-400 p-3 text-white transition-transform hover:-translate-y-1"
+			@click="template.editing = true"
+		>
 			go work on it
+			<Icon icon="material-symbols:edit-rounded" class="h-6 w-6" />
 		</button>
 	</div>
 </template>
