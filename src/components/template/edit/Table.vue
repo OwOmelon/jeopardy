b23d9ef5f6ac02b1ab28038b15d8ae52cc8d4b71
melon <cccaaarrrlll99@gmail.com>
2023-10-25 15:53:37 +0800

    refactored

--- src/components/template/edit/Table.vue
+++ src/components/template/edit/Table.vue
@@ -1,3 +1,80 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, computed } from "vue";
+import { useTemplateStore } from "@/stores/template";
 
-<template></template>
+import TRPoints from "./TRPoints.vue";
+import TRCategories from "./TRCategories.vue";
+import TD from "./TD.vue";
+import TDEditor from "./TDEditor.vue";
+import ModalWrapper from '@/components/ModalWrapper.vue'
+
+import type { RowID, Column } from "@/stores/template";
+
+const template = useTemplateStore();
+
+const TDHovered = ref<{
+	row: RowID | null;
+	column: Column["id"] | null;
+}>({
+	row: null,
+	column: null,
+});
+
+const horizontalTrTemplateColumns = computed<string>(() => {
+	return `repeat(${template.categoriesDisplay.length}, 140px)`;
+});
+
+function TDMouseEnter(row: RowID, column: Column["id"]): void {
+	TDHovered.value.row = row;
+	TDHovered.value.column = column;
+}
+
+function TDMouseLeave(): void {
+	TDHovered.value.row = null;
+	TDHovered.value.column = null;
+}
+</script>
+
+<template>
+	<table :class="['grid w-full overflow-auto p-8']">
+		<TRPoints v-if="template.editing" />
+		<TRCategories />
+
+		<div class="col-start-2 row-start-2 flex flex-col gap-3">
+			<TransitionGroup
+				tag="tr"
+				:name="template.editing ? 'slide' : 'disabled'"
+				v-for="(rowValue, rowKey, rowIndex) in template.tableDisplay"
+				:key="rowKey"
+				class="relative grid gap-3"
+			>
+				<template
+					v-for="(cellValue, cellKey, columnIndex) in rowValue"
+					:key="cellKey"
+				>
+					<TD
+						:cell="cellValue"
+						:hovered="rowKey === TDHovered.row && cellKey === TDHovered.column"
+						@on-mouse-enter="TDMouseEnter"
+						@on-mouse-leave="TDMouseLeave"
+					/>
+				</template>
+			</TransitionGroup>
+		</div>
+	</table>
+
+	<ModalWrapper :show="template.activeCell ? true : false">
+		<TDEditor />
+	</ModalWrapper>
+</template>
+
+<style scoped lang="postcss">
+:deep(tr) {
+	@apply grid;
+	grid-template-columns: v-bind(horizontalTrTemplateColumns);
+}
+
+:deep(.cell) {
+	@apply rounded border-y-2 border-transparent bg-white shadow !shadow-black/30 transition-colors;
+}
+</style>
