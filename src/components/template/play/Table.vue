e7741c73b27131c640ccccada7c35e4f5f21f07a
melon <cccaaarrrlll99@gmail.com>
2023-10-25 14:45:57 +0800

    renamed component: Template -> Table

---
+++ src/components/template/play/Table.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { useTemplateStore } from "@/stores/template";
+
+const template = useTemplateStore();
+</script>
+
+<template>
+	<div>
+		<h1 class="template-name">{{template.name}}</h1>
+	</div>
+</template>
