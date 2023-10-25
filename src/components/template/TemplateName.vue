f95a7e3a93f4e8bd6020fc13e8c3cafbb640a976
melon <cccaaarrrlll99@gmail.com>
2023-10-25 16:50:32 +0800

    wrapped TextBox in <caption>

--- src/components/template/TemplateName.vue
+++ src/components/template/TemplateName.vue
@@ -17,16 +17,22 @@ watch(
 </script>
 
 <template>
-	<TextBox
-		v-model="textInput"
-		placeholder="insert template name"
-		focus-classes="!border-b-red-400"
-		:disabled="!template.editing"
-		blur-on-keydown-enter
-		:class="[
-			{ 'border-y-2 border-transparent border-b-stone-600': template.editing },
-			'mx-auto w-fit min-w-[12em] p-2 text-center text-3xl font-bold transition-colors',
-		]"
-		@blur="template.name = textInput"
-	/>
+	<caption
+		class="mx-auto block w-fit min-w-[12em] p-2 text-center text-3xl font-bold"
+	>
+		<TextBox
+			v-model="textInput"
+			placeholder="insert template name"
+			focus-classes="!border-b-red-400"
+			:disabled="!template.editing"
+			blur-on-keydown-enter
+			:class="[
+				{
+					'border-y-2 border-transparent border-b-stone-600': template.editing,
+				},
+				'transition-colors',
+			]"
+			@blur="template.name = textInput"
+		/>
+	</caption>
 </template>
