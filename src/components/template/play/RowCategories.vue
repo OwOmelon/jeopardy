610f0b27325059a7839247eeba58123d824a00e9
melon <cccaaarrrlll99@gmail.com>
2023-10-26 12:26:32 +0800

    new component

---
+++ src/components/template/play/RowCategories.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useTemplateStore } from "@/stores/template";
+
+const template = useTemplateStore();
+</script>
+
+<template>
+	<tr>
+		<th v-for="column in template.columns" class="cell cell-width cell-padding">
+			{{ column.category || column.id }}
+		</th>
+	</tr>
+</template>
