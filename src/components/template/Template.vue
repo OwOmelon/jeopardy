637fdd4e5218074d1151a360404c0014f1dc3af7
melon <cccaaarrrlll99@gmail.com>
2023-10-26 12:56:19 +0800

    added colors for deep styles <td>

--- src/components/template/Template.vue
+++ src/components/template/Template.vue
@@ -28,7 +28,7 @@ const template = useTemplateStore();
 }
 
 :deep(td) {
-	@apply h-[9.5ex] cursor-pointer text-xs;
+	@apply h-[9.5ex] cursor-pointer bg-stone-300 text-xs text-stone-500;
 }
 
 :deep(.td-rise) {
