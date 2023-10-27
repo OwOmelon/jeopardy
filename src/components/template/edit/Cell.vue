c59607a4b9bc7a0bb8d62271c767b40eb6514cf4
melon <cccaaarrrlll99@gmail.com>
2023-10-27 11:25:56 +0800

    cleanup

--- src/components/template/edit/Cell.vue
+++ src/components/template/edit/Cell.vue
@@ -18,28 +18,7 @@ const emit = defineEmits<{
 	edit: [];
 }>();
 
-const dynamicStyles = computed<string>(() => {
-	const isMissingData = template.cellHasMissingData(props.row, props.column);
-
-	const playStyles = !template.editing
-		? `playing ${props.answeredBy !== undefined ? "answered" : ""} ${
-				isMissingData ? "pointer-events-none" : ""
-		  }`
-		: "";
-
-	const rise = !(!template.editing && props.answeredBy !== undefined)
-		? "hover-rise"
-		: "";
-
-	return `${playStyles} ${isMissingData ? "missing-data" : ""} ${rise}`;
-});
-
 const textDisplay = computed<number | string>(() => {
-	if (!template.editing)
-		return template.cellHasMissingData(props.row, props.column)
-			? ""
-			: props.points;
-
 	const question = props.question || "Add Question";
 	const answer = props.answer || "Add Answer";
 
@@ -49,7 +28,13 @@ const textDisplay = computed<number | string>(() => {
 
 <template>
 	<td
-		:class="[dynamicStyles, 'cell cell-width cell-padding td-rise']"
+		:class="[
+			{
+				'!bg-stone-500 !text-stone-100':
+					template.checkTableDataValues(props.row, props.column) !== 'complete',
+			},
+			'cell cell-width cell-padding td-rise',
+		]"
 		@mouseenter="emit('on-mouse-enter', props.row, props.column)"
 		@mouseleave="emit('on-mouse-leave')"
 		@click="emit('edit')"
@@ -59,21 +44,3 @@ const textDisplay = computed<number | string>(() => {
 		</p>
 	</td>
 </template>
-
-<style scoped lang="postcss">
-.missing-data {
-	@apply !bg-stone-500 !text-stone-100;
-}
-
-.playing {
-	@apply grid place-items-center hover:bg-red-400 hover:text-white;
-}
-
-.playing p {
-	@apply text-lg font-bold;
-}
-
-.answered {
-	@apply opacity-0 hover:opacity-50;
-}
-</style>
