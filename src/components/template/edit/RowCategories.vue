78d3d3ae37c089f6347b191735d253ea043d37f2
melon <cccaaarrrlll99@gmail.com>
2023-10-30 14:10:44 +0800

    moved <hr> stuff from <RowCategories> to <RowHRs>

--- src/components/template/edit/RowCategories.vue
+++ src/components/template/edit/RowCategories.vue
@@ -9,27 +9,6 @@ import type { Column } from "@/stores/template";
 
 const template = useTemplateStore();
 
-const hrColors = computed<{ id: Column["id"]; color: string }[]>(() => {
-	return template.columns.map((column) => {
-		let emptyCells = 0;
-
-		template.rows.forEach((row) => {
-			if (template.checkTableDataValues(row, column.id) === "empty") {
-				emptyCells++;
-			}
-		});
-
-		const color =
-			emptyCells === 0
-				? "border-green-400"
-				: emptyCells === 5
-				? "border-red-400"
-				: "border-yellow-400";
-
-		return { id: column.id, color };
-	});
-});
-
 function changeColumnCategory(newName: string, id: Column["id"]): void {
 	const columnIndex = template.columns.findIndex((column) => column.id === id);
 	const column = template.columns[columnIndex];
@@ -57,13 +36,5 @@ function changeColumnCategory(newName: string, id: Column["id"]): void {
 				/>
 			</template>
 		</draggable>
-
-		<TransitionGroup tag="tr" name="list-slide-left" class="my-3">
-			<hr
-				v-for="hr in hrColors"
-				:key="hr.id"
-				:class="[hr.color, 'cell-width rounded-full border-t-2']"
-			/>
-		</TransitionGroup>
 	</thead>
 </template>
--- src/components/template/edit/RowHRs.vue
+++ src/components/template/edit/RowHRs.vue
@@ -1,3 +1,39 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from "vue";
+import { useTemplateStore } from "@/stores/template";
 
-<template></template>
+import type { Column } from "@/stores/template";
+
+const template = useTemplateStore();
+
+const hrColors = computed<{ id: Column["id"]; color: string }[]>(() => {
+	return template.columns.map((column) => {
+		let emptyCells = 0;
+
+		template.rows.forEach((row) => {
+			if (template.checkTableDataValues(row, column.id) === "empty") {
+				emptyCells++;
+			}
+		});
+
+		const color =
+			emptyCells === 0
+				? "border-green-400"
+				: emptyCells === 5
+				? "border-red-400"
+				: "border-yellow-400";
+
+		return { id: column.id, color };
+	});
+});
+</script>
+
+<template>
+	<TransitionGroup tag="tr" name="list-slide-left" class="my-3">
+		<hr
+			v-for="hr in hrColors"
+			:key="hr.id"
+			:class="[hr.color, 'cell-width rounded-full border-t-2']"
+		/>
+	</TransitionGroup>
+</template>
