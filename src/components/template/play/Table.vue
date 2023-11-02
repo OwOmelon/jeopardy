dd92510ba33995933a66f0e3699d0cb537382b5e
melon <cccaaarrrlll99@gmail.com>
2023-11-02 13:14:21 +0800

    removed computed properties filteredColumns and filteredCompleteTable

--- src/components/template/play/Table.vue
+++ src/components/template/play/Table.vue
@@ -1,8 +1,5 @@
 <script setup lang="ts">
-import { computed } from "vue";
 import { useTemplateStore } from "@/stores/template";
-
-import type { Column, CompleteTable } from "@/stores/template";
 
 import { Icon } from "@iconify/vue";
 import RowCategories from "./RowCategories.vue";
@@ -11,34 +8,14 @@ import TableDataReveal from "./td_reveal/TableDataReveal.vue";
 import ModalWrapper from "@/components/ModalWrapper.vue";
 
 const template = useTemplateStore();
-
-const filteredColumns = computed<Column[]>(() => {
-	return template.columns.filter(
-		(column) => column.category || !template.columnIsEmpty(column.id),
-	);
-});
-
-const filteredCompleteTable = computed<CompleteTable>(() => {
-	return template.rows.reduce((rows, row) => {
-		return {
-			...rows,
-			[row]: filteredColumns.value.reduce((columns, column) => {
-				return {
-					...columns,
-					[column.id]: template.completeTable[row][column.id],
-				};
-			}, {}),
-		};
-	}, {});
-});
 </script>
 
 <template>
-	<table class="grid gap-3" v-if="filteredColumns.length">
-		<RowCategories :columns="filteredColumns" />
+	<table class="grid gap-3" v-if="template.filteredColumns.length">
+		<RowCategories />
 
 		<tr
-			v-for="(rowValue, rowKey, rowIndex) in filteredCompleteTable"
+			v-for="(rowValue, rowKey, rowIndex) in template.filteredCompleteTable"
 			:key="rowKey"
 		>
 			<TableData
