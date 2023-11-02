ab6110d9391fba646a74b05c492790054ebb1411
melon <cccaaarrrlll99@gmail.com>
2023-11-02 13:13:56 +0800

    removed props. use template.filteredColumns for v-for instead

--- src/components/template/play/RowCategories.vue
+++ src/components/template/play/RowCategories.vue
@@ -1,18 +1,13 @@
 <script setup lang="ts">
 import { useTemplateStore } from "@/stores/template";
-import type { Column } from "@/stores/template";
 
 const template = useTemplateStore();
-
-const props = defineProps<{
-	columns: Column[]
-}>()
 </script>
 
 <template>
 	<tr>
 		<th
-			v-for="column in props.columns"
+			v-for="column in template.filteredColumns"
 			class="cell cell-width cell-padding bg-red-400 text-white"
 		>
 			{{ column.category || column.id }}
