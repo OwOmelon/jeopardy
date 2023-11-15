1de1eb76d78ed65156cee8be1783a8323b2baa16
melon <cccaaarrrlll99@gmail.com>
2023-11-15 12:13:09 +0800

    new store for game progress

---
+++ src/stores/game_progress.ts
@@ -0,0 +1,92 @@
+import { ref, computed, watch } from "vue";
+import { defineStore } from "pinia";
+import { useTemplateStore } from './template'
+import { useGuestsStore } from './guests'
+
+import type { RowID, Column } from "./template";
+import type { Guest } from "./guests";
+
+export type GameProgressValue = {
+	failedToAnswer: Guest["id"][];
+	successfullyAnswered: Guest["id"] | null;
+};
+
+export type GameProgress = {
+	[key: RowID]: {
+		[key: Column["id"]]: GameProgressValue;
+	};
+};
+
+export const useGameProgressStore = defineStore("game-progress", () => {
+	const template = useTemplateStore();
+	const guests = useGuestsStore();
+
+	const progress = ref<GameProgress>(
+		fetchGameProgressFromLocalStorage() || {},
+	);
+	const resetGameProgressWarning = ref<boolean>(false);
+
+	function updateGameProgress(
+		failedToAnswer: GameProgressValue["failedToAnswer"],
+		successfullyAnswered: GameProgressValue["successfullyAnswered"],
+	) {
+		const row = progress.value?.[template.activeCell!.row] ?? {};
+
+		row[template.activeCell!.column!] = {
+			failedToAnswer,
+			successfullyAnswered,
+		};
+		progress.value[template.activeCell!.row] = row;
+	}
+
+	function updateGuestsLegitimatePoints(): void {
+		guests.list.forEach((guest) => {
+			guest.points.legitimate = 0;
+		});
+
+		const gameProgressRows = Object.keys(progress.value) as RowID[];
+
+		gameProgressRows.forEach((row) => {
+			const pointsToAssign = template.points[template.rows.indexOf(row)];
+			const gamePRogressColumns = Object.keys(
+				progress.value[row],
+			) as Column["id"][];
+
+			gamePRogressColumns.forEach((column) => {
+				const gameProgressValue = progress.value[row][column];
+
+				gameProgressValue.failedToAnswer.forEach((id) => {
+					guests.editGuestPoints(id, -pointsToAssign, "legitimate", true);
+				});
+
+				guests.editGuestPoints(
+					gameProgressValue?.successfullyAnswered ?? "",
+					pointsToAssign,
+					"legitimate",
+					true,
+				);
+			});
+		});
+	}
+
+	watch(
+		progress,
+		(progress) => {
+			updateGuestsLegitimatePoints();
+			localStorage.setItem("play-progress", JSON.stringify(progress));
+		},
+		{ deep: true },
+	);
+
+	function fetchGameProgressFromLocalStorage(): GameProgress | null {
+		const savedProgress = localStorage.getItem("play-progress");
+
+		return savedProgress ? JSON.parse(savedProgress) : null;
+	}
+
+	return {
+		progress,
+		resetGameProgressWarning,
+		updateGameProgress
+	}
+});
