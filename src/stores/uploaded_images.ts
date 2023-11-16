1c6cdbebd239b7f3353d87fa8ba88d5f0d7983ea
melon <cccaaarrrlll99@gmail.com>
2023-11-16 15:18:11 +0800

    added function for adding images

--- src/stores/uploaded_images.ts
+++ src/stores/uploaded_images.ts
@@ -15,7 +15,22 @@ export type UploadedImages = {
 export const useUploadedImagesStore = defineStore("uploaded-images", () => {
 	const images = ref<UploadedImages>({});
 
+	function addImage(
+		src: string,
+		type: "question" | "answer",
+		row: RowID,
+		column: Column["id"],
+	) {
+		const imageRow = images.value?.[row] ?? {};
+		const imageCell = imageRow?.[column] ?? { question: "", answer: "" };
+
+		imageCell[type] = src;
+		imageRow[column] = imageCell
+		images.value[row] = imageRow;
+	}
+
 	return {
 		images,
+		addImage,
 	};
 });
