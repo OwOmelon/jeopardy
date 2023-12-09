86f619dca7f5154f76dff53469a100ea836596e5
melon <cccaaarrrlll99@gmail.com>
2023-12-09 14:32:10 +0800

    new component. alert modal for template errors

---
+++ src/components/template/TemplatePropertyErrorAlert.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import type { TemplateErrors } from "@/composables/check_template_for_errors";
+
+import WarningModal from "../WarningModal.vue";
+
+const props = defineProps<{
+	header: string;
+	paragraph: string[];
+	errors: TemplateErrors;
+}>();
+
+const emit = defineEmits<{
+	close: [];
+}>();
+</script>
+
+<template>
+	<WarningModal
+		:header="header"
+		:paragraph="paragraph"
+		hide-confirm-btn
+		@close="emit('close')"
+	>
+		<div v-for="(errorList, property, index) in errors" :key="property">
+			<span class="font-bold tracking-wide">{{ property }}: </span>
+			<ol class="ml-8 list-disc">
+				<li v-for="error in errorList">{{ error }}</li>
+			</ol>
+		</div>
+	</WarningModal>
+</template>
