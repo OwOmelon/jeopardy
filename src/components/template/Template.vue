943281f5054688439d66427999c88810c97a560f
melon <cccaaarrrlll99@gmail.com>
2023-12-10 12:44:54 +0800

    warn user when local storage template has errors

--- src/components/template/Template.vue
+++ src/components/template/Template.vue
@@ -1,15 +1,33 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import { useTemplateStore } from "@/stores/template";
+import { downloadTemplate } from "@/composables/download_template";
+
+import { Icon } from "@iconify/vue";
 
 import TemplateName from "./TemplateName.vue";
 import Content_Edit from "./edit/Content.vue";
 import Content_Play from "./play/Content.vue";
+import ModalWrapper from "../ModalWrapper.vue";
+import TemplatePropertyErrorAlert from "./TemplatePropertyErrorAlert.vue";
 
 const template = useTemplateStore();
+const downloadLocalStorageTemplate = ref<boolean>(false);
+
+function onTemplatePropertyErrorAlertClose(): void {
+	const localStorageTemplate = template.fetchTemplateFromLocalStorage();
+
+	template.localStorageTemplateErrors = null;
+	template.templateData = template.createTemplate();
+
+	if (downloadLocalStorageTemplate.value && localStorageTemplate) {
+		downloadTemplate(localStorageTemplate);
+	}
+}
 </script>
 
 <template>
-	<div class="w-full mt-10/">
+	<div class="w-full">
 		<TemplateName />
 		<Transition
 			:name="template.editing ? 'fade-slide-right' : 'fade-slide-left'"
@@ -18,6 +36,48 @@ const template = useTemplateStore();
 			<Content_Edit v-if="template.editing" />
 			<Content_Play v-else />
 		</Transition>
+
+		<ModalWrapper :show="template.localStorageTemplateErrors !== null">
+			<TemplatePropertyErrorAlert
+				header="!! LOCAL STORAGE TEMPLATE ERROR"
+				:paragraph="[
+					'Errors have been found in your local storage template.',
+					'The only way this could have happened is that it was manually tampered with.',
+					'Your Jeopardy template will be reset once this window closes.',
+				]"
+				:errors="template.localStorageTemplateErrors!"
+				@close="onTemplatePropertyErrorAlertClose"
+			>
+				<label
+					class="flex cursor-pointer items-center gap-3"
+				>
+					<input
+						v-model="downloadLocalStorageTemplate"
+						type="checkbox"
+						class="fixed top-full hidden"
+					/>
+
+					<div
+						:class="[
+							downloadLocalStorageTemplate ? 'bg-white' : 'bg-red-400',
+							'aspect-square w-8 rounded border-2 border-white shadow shadow-black/30',
+						]"
+					>
+						<Icon
+							icon="material-symbols:check-small"
+							:class="[
+								{ hidden: !downloadLocalStorageTemplate },
+								'h-full w-full scale-125 text-red-400',
+							]"
+						/>
+					</div>
+
+					<span class="tracking wide font-bold"
+						>download local storage template</span
+					>
+				</label>
+			</TemplatePropertyErrorAlert>
+		</ModalWrapper>
 	</div>
 </template>
 
