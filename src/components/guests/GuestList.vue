e5e2eaba216474b4eb5e60f00bcec7756de944f4
melon <cccaaarrrlll99@gmail.com>
2023-12-14 11:44:58 +0800

    remove unnecessary conditional display wrapper div

--- src/components/guests/GuestList.vue
+++ src/components/guests/GuestList.vue
@@ -1,23 +1,19 @@
 <script setup lang="ts">
 import { useGuestsStore } from "@/stores/guests";
-import { useTemplateStore } from "@/stores/template";
 
 import Guest from "./Guest.vue";
 
 const guests = useGuestsStore();
-const template = useTemplateStore();
 </script>
 
 <template>
-	<div v-if="!template.editing && guests.list.length" class="overflow-hidden">
-		<ul class="flex flex-wrap justify-center gap-3">
-			<Guest
-				v-for="guest in guests.list"
-				:id="guest.id"
-				:name="guest.name"
-				:points="guest.points"
-				@edit-points="guests.editGuestPoints(guest.id, $event, 'illegitimate')"
-			/>
-		</ul>
-	</div>
+	<ul class="flex flex-wrap justify-center gap-3 pb-1">
+		<Guest
+			v-for="guest in guests.list"
+			:id="guest.id"
+			:name="guest.name"
+			:points="guest.points"
+			@edit-points="guests.editGuestPoints(guest.id, $event, 'illegitimate')"
+		/>
+	</ul>
 </template>
