830dd72f9b402862fbe0c78c4d395f94244d8f19
melon <cccaaarrrlll99@gmail.com>
2024-02-07 14:13:44 +0800

    add 'tr-flex' class to play row categories

--- src/components/template/play/RowCategories.vue
+++ src/components/template/play/RowCategories.vue
@@ -7,7 +7,7 @@ const props = defineProps<{
 </script>
 
 <template>
-	<tr class="items-end">
+	<tr class="tr-flex items-end">
 		<th
 			v-for="(category, column, index) in columns"
 			class="cell cell-width cell-padding bg-red-400 text-white"
