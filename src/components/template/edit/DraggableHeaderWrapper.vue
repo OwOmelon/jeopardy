8e32478a57071cd2a088b8990586e1e58f75d0df
melon <cccaaarrrlll99@gmail.com>
2024-02-15 15:30:52 +0800

    remove Drag and Drop API logic - instead of using the drag and drop api, use mouse events - reason: drag and drop api doesn't work for mobile

--- src/components/template/edit/DraggableHeaderWrapper.vue
+++ src/components/template/edit/DraggableHeaderWrapper.vue
@@ -2,6 +2,8 @@
 import { ref, onMounted } from "vue";
 import { arrSwap } from "@/composables/array_swap";
 
+type Attr = `drag-${typeof props.group}-${number}`;
+
 const emit = defineEmits<{
 	dragstart: [];
 	dragend: [];
@@ -20,39 +22,50 @@ const wrapper = ref<any>(null);
 const dragFrom = ref<number | null>(null);
 const dropTo = ref<number | null>(null);
 
-function onDragStart(attr: string): void {
+function onDragStart(attr: Attr): void {
 	const index = findIndex(attr);
 
 	dragFrom.value = index;
+
 	emit("dragstart");
 }
 
-function onDragEnter(attr: string): void {
+function onDragEnter(e: MouseEvent): void {
 	if (dragFrom.value === null) return;
+
+	const attr = getDragAttribute(e.target as HTMLElement);
+
+	if (attr === null) return;
 
 	const index = findIndex(attr);
 
+	if (index === dragFrom.value) return;
+
 	dropTo.value = index;
+	document.body.style.cursor = "cell";
 }
 
-function onDrop(): void {
+function onDragLeave(): void {
 	if (dragFrom.value === null || dropTo.value === null) return;
 
-	const swappedModelValueEntries = arrSwap(
-		Object.entries(obj.value),
-		dragFrom.value,
-		dropTo.value,
-	);
-
-	const swappedModelValue = Object.fromEntries(
-		swappedModelValueEntries,
-	) as typeof obj.value;
-
-	obj.value = swappedModelValue;
+	dropTo.value = null;
+	document.body.style.cursor = "no-drop";
 }
 
-function onDragEnd(el: Element): void {
-	el.removeAttribute("draggable");
+function onDragEnd(): void {
+	if (dragFrom.value !== null && dropTo.value !== null) {
+		const swappedModelValueEntries = arrSwap(
+			Object.entries(obj.value),
+			dragFrom.value,
+			dropTo.value,
+		);
+
+		const swappedModelValue = Object.fromEntries(
+			swappedModelValueEntries,
+		) as typeof obj.value;
+
+		obj.value = swappedModelValue;
+	}
 
 	dragFrom.value = null;
 	dropTo.value = null;
@@ -62,51 +75,68 @@ function onDragEnd(el: Element): void {
 
 // --------------------
 
-function onHandleMouseDown(el: Element) {
-	el.setAttribute("draggable", "true");
+function startDragOperations(e: MouseEvent, attr: Attr): void {
+	e.preventDefault();
+
+	onDragStart(attr);
+
+	getChildren()!.forEach((child) => {
+		child.addEventListener("mouseenter", onDragEnter);
+		child.addEventListener("mouseleave", onDragLeave);
+	});
+
+	document.body.style.cursor = "no-drop";
+	window.addEventListener("mouseup", stopDragOperations);
+}
+
+function stopDragOperations(): void {
+	onDragEnd();
+
+	getChildren()!.forEach((child) => {
+		child.removeEventListener("mouseenter", onDragEnter);
+		child.removeEventListener("mouseleave", onDragLeave);
+	});
+
+	document.body.style.cursor = "";
+	window.removeEventListener("mouseup", stopDragOperations);
 }
 
 // --------------------
 
-function findIndex(attr: string): number | null {
+function findIndex(attr: Attr): number | null {
 	const children = getChildren();
 
 	return children !== null
-		? children.findIndex((child) => child.hasAttribute(attr))
+		? children.findIndex((child) => child.getAttribute("drag-item") === attr)
 		: null;
 }
 
-function getChildren(): Element[] | null {
-	const el = getWrapperEl();
-
-	return el !== null ? Array.from(el.children) : null;
+function getDragAttribute(el: HTMLElement): Attr | null {
+	return (el.getAttribute("drag-item") as Attr) ?? null;
 }
 
-function getWrapperEl(): Element | null {
+function getChildren(): HTMLElement[] | null {
+	const wrapperEl = getWrapperEl();
+
+	return wrapperEl !== null
+		? (Array.from(wrapperEl.children) as HTMLElement[])
+		: null;
+}
+
+function getWrapperEl(): HTMLElement | null {
 	return wrapper.value?.$el ?? null;
 }
 
 onMounted(() => {
 	setTimeout(() => {
-		const el = getWrapperEl()!;
-		const children = Array.from(el.children);
+		getChildren()!.forEach((child, index) => {
+			const attr: Attr = `drag-${props.group}-${index}`;
+			const handle = child.getElementsByClassName(
+				props.handle,
+			)[0] as HTMLElement;
 
-		el.addEventListener("drop", () => onDrop());
-		el.addEventListener("dragover", (e) => {
-			e.preventDefault();
-		});
-
-		children.forEach((child, index) => {
-			const attr = `drag-${props.group}-${index}`;
-			const handle = child.getElementsByClassName(props.handle)[0];
-
-			handle.addEventListener("mousedown", () => onHandleMouseDown(child));
-
-			child.setAttribute(attr, "");
-
-			child.addEventListener("dragstart", () => onDragStart(attr));
-			child.addEventListener("dragenter", () => onDragEnter(attr));
-			child.addEventListener("dragend", () => onDragEnd(child));
+			handle.addEventListener("mousedown", (e) => startDragOperations(e, attr));
+			child.setAttribute("drag-item", attr);
 		});
 	}, 100);
 });
@@ -119,7 +149,7 @@ onMounted(() => {
 				:value="value"
 				:property="key"
 				:dragging="index === dragFrom"
-				:dropTo="index === dropTo && index !== dragFrom"
+				:dropTo="index === dropTo"
 			/>
 		</template>
 	</TransitionGroup>
