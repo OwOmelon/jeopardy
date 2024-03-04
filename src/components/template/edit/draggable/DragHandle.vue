25e51e45dae2b663f32253fb2ac12fd4dd8cf633
melon <cccaaarrrlll99@gmail.com>
2024-03-04 13:57:11 +0800

    set cursor to 'grabbing' when active

--- src/components/template/edit/draggable/DragHandle.vue
+++ src/components/template/edit/draggable/DragHandle.vue
@@ -10,7 +10,7 @@ const props = defineProps<{
 		:class="[
 			rotate ? 'px-1 py-3' : 'px-3 py-1',
 			{ 'flex-col': props.rotate },
-			'handle lg:opacity-0 absolute left-0 top-0 flex -translate-x-1/2 -translate-y-1/2 cursor-grab rounded bg-stone-300 shadow shadow-black/30 transition-opacity group-hover:opacity-100',
+			'handle absolute left-0 top-0 flex -translate-x-1/2 -translate-y-1/2 cursor-grab rounded bg-stone-300 shadow shadow-black/30 transition-opacity active:cursor-grabbing group-hover:opacity-100 lg:opacity-0',
 		]"
 		@click.stop
 	>
