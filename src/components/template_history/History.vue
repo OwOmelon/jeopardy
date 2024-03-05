0bf5ad7051b50e2b4311203f8cc7023ac80323fb
melon <cccaaarrrlll99@gmail.com>
2024-03-05 14:42:04 +0800

    add additional styles to undo/redo buttons

--- src/components/template_history/History.vue
+++ src/components/template_history/History.vue
@@ -74,7 +74,7 @@ watch(hide, () => {
 		<div class="absolute left-full top-0 ml-2 flex items-start gap-3 text-sm">
 			<button
 				type="button"
-				class="grid aspect-square place-items-center rounded bg-black/50 !p-3 backdrop-blur !text-xl"
+				class="grid aspect-square place-items-center rounded bg-black/50 !p-3 !text-xl backdrop-blur"
 				@click="hide = !hide"
 			>
 				<IconHamburger />
@@ -105,6 +105,6 @@ ul {
 }
 
 button {
-	@apply rounded bg-stone-600 px-2 py-1 text-stone-200 disabled:cursor-not-allowed disabled:bg-stone-700 disabled:text-stone-600;
+	@apply rounded bg-stone-600 px-2 py-1 text-stone-200 transition-[background-color,_color,_transform] hover:bg-stone-200 hover:text-stone-600 active:scale-90 disabled:cursor-not-allowed disabled:bg-stone-700 disabled:text-stone-600;
 }
 </style>
