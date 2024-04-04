9789d05bd5a7424e790b41fab1929855060aa38f
melon <cccaaarrrlll99@gmail.com>
2024-04-04 13:39:24 +0800

    component wrapper for 'height-auto' transitions

---
+++ src/components/HeightAutoTransitionWrapper.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const props = defineProps<{
+	show: boolean;
+}>();
+</script>
+
+<template>
+	<Transition name="height-auto">
+		<div v-if="show" class="grid">
+			<div class="overflow-hidden">
+				<slot />
+			</div>
+		</div>
+	</Transition>
+</template>
