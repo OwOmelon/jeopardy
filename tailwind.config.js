7302a76f57e19db2abf106f8c209561521080383
melon <cccaaarrrlll99@gmail.com>
2024-04-05 15:05:11 +0800

    add custom class 'shadow-subtle' - 'shadow !shadow-black/30

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
-  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [
+    plugin(function ({ addComponents }) {
+      addComponents({
+        ".shadow-subtle": {
+          boxShadow: "0 1px 3px 0 rgb(0 0 0 / 0.3), 0 1px 2px -1px rgb(0 0 0 / 0.3)",
+        },
+      });
+    }),
+  ],
+};
