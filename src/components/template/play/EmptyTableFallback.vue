e48cff278b776d08d2af8fac89ef3243093921b7
melon <cccaaarrrlll99@gmail.com>
2024-04-10 18:26:17 +0800

    text update: change word 'table' -> 'template'

--- src/components/template/play/EmptyTableFallback.vue
+++ src/components/template/play/EmptyTableFallback.vue
@@ -7,7 +7,7 @@ const template = useTemplateStore();
 
 <template>
 	<div class="mx-auto my-10 w-fit rounded bg-stone-50 p-5 shadow-md">
-		<p class="text-xl font-bold text-stone-600">your table is empty D:</p>
+		<p class="text-xl font-bold text-stone-600">your template is empty D:</p>
 		<button
 			type="button"
 			class="mx-auto mt-5 flex items-center gap-2 rounded bg-red-400 p-2 text-sm text-white shadow transition-transform hover:-translate-y-1"
