0708a198801835df9ebbc88fb4ec4c51eab46b32
melon <cccaaarrrlll99@gmail.com>
2024-04-10 18:09:01 +0800

    set text color and increase parent padding

--- src/components/template/play/EmptyTableFallback.vue
+++ src/components/template/play/EmptyTableFallback.vue
@@ -6,8 +6,8 @@ const template = useTemplateStore();
 </script>
 
 <template>
-	<div class="mx-auto my-10 w-fit rounded bg-stone-50 p-3 shadow-md">
-		<p class="text-xl font-bold">your table is empty D:</p>
+	<div class="mx-auto my-10 w-fit rounded bg-stone-50 p-5 shadow-md">
+		<p class="text-xl font-bold text-stone-600">your table is empty D:</p>
 		<button
 			type="button"
 			class="mx-auto mt-5 flex items-center gap-2 rounded bg-red-400 p-2 text-sm text-white shadow transition-transform hover:-translate-y-1"
