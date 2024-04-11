e26c55f734054c4c4ea1ac8544d8d6d22b775ea0
melon <cccaaarrrlll99@gmail.com>
2024-04-11 14:37:56 +0800

    remove dynamic styles. colors will now remain static

--- src/components/MainMenuModal/PauseBtn.vue
+++ src/components/MainMenuModal/PauseBtn.vue
@@ -1,22 +1,15 @@
 <script setup lang="ts">
-import { useTemplateStore } from "@/stores/template";
 import { useMainMenuStore } from "@/stores/mainmenu";
 
 import IconPause from "~icons/solar/pause-bold";
 
-const template = useTemplateStore();
 const mainmenu = useMainMenuStore();
 </script>
 
 <template>
 	<button
 		type="button"
-		:class="[
-			template.editing
-				? 'bg-red-400 text-white'
-				: 'bg-stone-500 text-stone-300',
-			'fixed right-2 top-2 grid aspect-square place-items-center rounded p-3 text-xl transition-[background-color,_color,_transform] hover:scale-110',
-		]"
+		class="shadow-subtle fixed right-2 top-2 grid aspect-square place-items-center rounded bg-red-400 p-3 text-xl text-white transition-[background-color,_color,_transform] hover:scale-110"
 		@click="mainmenu.show = !mainmenu.show"
 	>
 		<IconPause />
