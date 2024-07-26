497f18d5c3019ac0350c1d27936a92f584017cfd
melon <cccaaarrrlll99@gmail.com>
2024-07-26 13:22:32 +0800

    remove "revealProgressLimitInjectionKey" - provide both "revealProgress" & "revealProgressLimit" refs as read only under "revealProgressInjectionKey" - provide functions for mutating ref "revealProgress" under key "updateRevealProgressInjectionKey"

--- src/components/template/play/td_reveal/reveal-progress-injection-key.ts
+++ src/components/template/play/td_reveal/reveal-progress-injection-key.ts
@@ -1,4 +1,12 @@
 import { type Ref, type InjectionKey } from "vue";
 
-export const revealProgressInjectionKey = Symbol() as InjectionKey<Ref<number>>;
-export const revealProgressLimitInjectionKey = Symbol() as InjectionKey<Ref<number>>;
+// PROVIDED PROPERTIES CAN BE FOUND IN "TableDataReveal.vue"
+export const revealProgressInjectionKey = Symbol() as InjectionKey<{
+	revealProgress: Readonly<Ref<number>>;
+	revealProgressLimit: Readonly<Ref<number>>;
+}>;
+
+export const updateRevealProgressInjectionKey = Symbol() as InjectionKey<{
+	revertProgress: () => void;
+	advanceProgress: () => void;
+}>;
