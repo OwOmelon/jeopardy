878defa18348e4dd315efb49b1d9058cc4d86914
melon <cccaaarrrlll99@gmail.com>
2024-07-26 12:49:03 +0800

    create reusable custom injection key for computed property "revealProgressLimit" - computed property will be provided from component <TableDataReveal>

--- src/components/template/play/td_reveal/reveal-progress-injection-key.ts
+++ src/components/template/play/td_reveal/reveal-progress-injection-key.ts
@@ -1,3 +1,4 @@
 import { type Ref, type InjectionKey } from "vue";
 
 export const revealProgressInjectionKey = Symbol() as InjectionKey<Ref<number>>;
+export const revealProgressLimitInjectionKey = Symbol() as InjectionKey<Ref<number>>;
